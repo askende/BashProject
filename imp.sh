3fc3eade1346f6279ecb18cade4529112012e75f
Anisa Skenderovic <askender@ulb.ac.be>
2019-11-11 17:11:57 +0100

    Copie et changement de nom

--- imp.sh
+++ imp.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
 # Auteur : Anisa Skenderovic (000457599)
+nouveauNom()
+{
+  local CIBLE=$1
+  echo $CIBLE
+  local FILE=$2
+  echo $FILE
+  local LIGNE=$4
+  echo $LIGNE
+  local NAME=${LIGNE:1:-2} # Garde que le nom
+  mkdir -p $CIBLE/$NAME
+  cp $FILE $CIBLE/$NAME
+  local NEWFILE=${FILE##*/} #Retrait du chemin
+  mv $CIBLE/$NAME/$NEWFILE $CIBLE/$NAME/$NAME.pgn
+}
+
 
 migration()
 {
+  CibleRep=$2
   if [ -f $1 ]    # Le paramètre donné est un fichier
   	then
     local FILE=$1
     local newNameBlacks=""
-    cat $FILE | while  read ligne ; do
-      if [[ $ligne =~ Black ]]
+    cat $FILE | while  read LIGNE ; do
+      echo "$LIGNE hors if"
+      if [[ $LIGNE =~ Black ]]
       then
-          queryValue $ligne
-          echo $ligne
-          #mv $newNameBlacks $newNameBlacks $name
-
+        echo "here $LIGNE"
+        Elo=`echo ${LIGNE} | grep "BlackElo" | wc -l`
+        if [[ ${Elo} == 0 ]] # Vérifie que ce ne soit pas le classement ELo mais le nom
+        then
+          nouveauNom $CibleRep $FILE $LIGNE
+        fi
       fi
-
+      if [[ $LIGNE =~ White ]]
+      then
+        echo "here $LIGNE"
+        Elo=`echo ${LIGNE} | grep "WhiteElo" | wc -l`
+        if [[ ${Elo} == 0 ]] # Vérifie que ce ne soit pas le classement ELo mais le nom
+        then
+          nouveauNom $CibleRep $FILE $LIGNE
+        fi
+      fi
     done
 
   else
     ALLREP=$(ls $1)
   	for REP in $ALLREP   # Parcours de chaque répertoire
   	do
-  	   migration $1/$REP # Appel récursif sur chaque répertoire
+  	   migration $1/$REP $CibleRep # Appel récursif sur chaque répertoire
   	done
 
   fi
@@ -31,14 +58,7 @@ migration()
 
 }
 
-queryValue()
-{
-  #echo $1 | cut -d'"' -f1
-  local name=$1
-  name=${name:6}
-  #$name=${$1%'"'*}
-  echo $name
-}
+
 
 SourceRep=$1 # chemin vers le répertoire source des fichiers PGN
 CibleRep=$2 # chemin vers le répertoire cible qui contiendra la nouvelle arborescence
@@ -46,6 +66,6 @@ PoubelleRep=$3 # chemin vers le répertoire poubelle qui contiendra les fichiers
 ConflitFile=$4 # chemin vers le fichier de conflits
 
 mkdir -p $CibleRep
-migration $SourceRep
+migration $SourceRep $CibleRep
 
 exit 0
